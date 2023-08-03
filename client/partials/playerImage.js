eef067dcc143924c256b45cb6d57c61f0d5dea9e
Sascha Dobschal <s.dobschal@right-basedonscience.e>
2023-08-03 22:00:47 +0200

    add 4 more avatars

--- client/partials/playerImage.js
+++ client/partials/playerImage.js
@@ -3,7 +3,7 @@
  * @returns {string}
  */
 export function renderPlayerImage (player) {
-  const index = player.id % 7 + 1
+  const index = player.id % 11 + 1
   const imageUrl = `assets/players/soccer_player-${index}.svg`
   return `
     <div class="player-image"  style="background-image: url('${imageUrl}')"></div>
