5f617546ea672adafa60a9ef3a50ee93597a6f38
Sascha Dobschal <sascha@dobschal.eu>
2026-07-25 16:31:14 +0530

    styling

--- client/pages/my-team.js
+++ client/pages/my-team.js
@@ -35,7 +35,7 @@ export class MyTeamPage extends TabbedPage {
           <nav class="nav nav-pills">
             <a class="nav-link ${!this.subPage ? 'active' : ''}" href="#my-team"><i class="fa fa-male"></i> ${t('myTeam.aTeam')}</a>
             <a class="nav-link ${this.subPage === 'youth' ? 'active' : ''}" href="#my-team?sub_page=youth"><i class="fa fa-child"></i> ${t('myTeam.youthTeam')}</a>
-            <a class="nav-link ${this.subPage === 'cards' ? 'active' : ''}" href="#my-team?sub_page=cards"><i class="fa fa-clone"></i> ${t('dashboard.tabCards')}${this.newCardCount > 0 ? ` <span class="badge bg-danger rounded-pill">${this.newCardCount}</span>` : ''}</a>
+            <a class="nav-link ${this.subPage === 'cards' ? 'active' : ''}" href="#my-team?sub_page=cards"><i class="fa fa-clone"></i> ${t('dashboard.tabCards')}${this.newCardCount > 0 ? ` <span class="badge bg-danger rounded-pill" style="position: absolute;transform: translateY(-26px) translateX(-5px);padding: 8px;text-align: center;">${this.newCardCount}</span>` : ''}</a>
             <a class="nav-link ${this.subPage === 'info' ? 'active' : ''}" href="#my-team?sub_page=info"><i class="fa fa-info-circle"></i> ${t('stadium.tabClubInfo')}</a>
           </nav>
           <a class="btn btn-sm btn-outline-secondary d-none d-lg-inline-block" href="#team?id=${this.data.team.id}">
@@ -93,9 +93,9 @@ export class MyTeamPage extends TabbedPage {
     }
   }
   get routeName () { return 'my-team' }
-  
+
   get defaultSubPageKey () { return 'ateam' }
-  
+
   createSubPage (key) {
     if (key === 'youth') return new YouthTeamPage(this)
     if (key === 'cards') return new ActionCards()
